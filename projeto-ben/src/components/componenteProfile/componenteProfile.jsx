416dc2f31f43dbdff2e067fa6078397af36af241
Anderson Martins <anderson77martins@outlook.com>
2021-07-02 23:01:14 -0300

    Refazendo o prjeto do zero

---
+++ projeto-ben/src/components/componenteProfile/componenteProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './cssProfile.css';
+
+export default props =>
+    <form className="formReact">
+        <h1>Profile</h1>
+        <label>
+            UserName
+            <input type="text" name="name" />
+        </label>
+    </form>
+
+   
--- projeto-ben/src/components/css/Menu.jsx
+++ projeto-ben/src/components/css/Menu.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './MenuCss_1.css'
 
+// eslint-disable-next-line
 export default props =>
     <div className="Menu">
         <div className="Menu-2">
-            <div className="Menu-3">
-                <div>Home</div>
-                {/*colocar o arquivo de profile aqui*/}
-                <div><a href="#">Profile</a></div>
-            </div>
-        </div>        
+            <div className="home">Home</div>
+            {/*colocar o arquivo de profile aqui*/}
+            <div className="profile"><link rel="import" href="#">Profile</link></div>
+        </div>
     </div>
